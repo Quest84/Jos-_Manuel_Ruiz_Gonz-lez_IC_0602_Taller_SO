52c46033eab03ee63b0eb2e4ba118a4b8b5249f7
QuestionSleep <Questionsleep123@gmail.com>
2021-05-19 08:55:56 -0500

    Terminado

--- 4_ExamenPractico/E2.c
+++ 4_ExamenPractico/E2.c
@@ -12,33 +12,50 @@ int main(){
 	}
 	
 	// Crear un puntero hacia un char de nombre punteroChar e inicializarlo con arrayChar
-	char *punteroChar = arrayChar;
-
-	//Crear un puntero hacia un char de nombre punteroCharDos e inicializarlo con la direccion del primer elemento (index) del array "arrayChar"
+	char *punteroChar;
+       	punteroChar = &arrayChar[0];
 	
-	char *punteroDosChar = &arrayChar[1];
+//	printf("\nDireccion de punteroChar = %p", &punteroChar);
+//	printf("\nContenido de PunteroChar = %c", punteroChar);
+	
+//	punteroChar = punteroChar + 1;
+//	printf("\nContenido de PunteroChar = %c", punteroChar);
+
+	
+	//Crear un puntero hacia un char de nombre punteroCharDos e inicializarlo con la direccion del primer elemento (index) del array "arrayChar"	
+	char *punteroCharDos;
+       	punteroCharDos = &arrayChar[0];
 
 	// Imprimir el contenido del primer elemento del array "arrayChar"
 	printf("\nContenido [0] de arrayChar = %c", arrayChar[0]);
 
 
-	// Imprimir el contenido del primer elemento del putnero punteroChar
-	printf("\nContenido del [0] de punteroChar = %c", );
+	// Imprimir el contenido del primer elemento del puntero punteroChar
+	printf("\nContenido del primer elemento de punteroChar = %c", *punteroChar);
 
 	// Imprimir el contenido del primer elemento del puntero punteroCharDos
-	printf("\nContenido del [0] de punteroCharDos = %c", );
+	printf("\nContenido del primer elemento de punteroCharDos = %c", *punteroCharDos);
 
 	// Imprimir el contenido del quinto elemento del array "arrayChar"
-	printf("\nContenido dl [5] de arrayChar = %c", arrayChar[4]);
+	printf("\nContenido del quinto elemento de arrayChar = %c", arrayChar[4]);
 
 	//Imprimir el contendo del quinto elemento del puntero "punteroChar" con aritmetica de punteros
-	printf("\nContenido del [5] de punteroChar con aritmetica de punteros = %c", );
+//	punteroChar = *(punteroChar + 4);
+	printf("\nContenido del quinto elemento de punteroChar con aritmetica de punteros = %c", *(punteroChar + 4));
 
 	// Imprimir el contenido del quinto elemento del puntero "punteroCharDos" con aritmética de punteros
-	printf("\nContenido del [5] de punteroCharDos con arimética de punteros = %", );
+//	punteroCharDos = *(punteroCharDos + 4);
+	printf("\nContenido del quinto elemento de punteroCharDos con arimética de punteros = %c", *(punteroCharDos + 4));
 
-	// Usar una estructura repetitiva para imprimir el contenido de todos los elementos de "punteroCharDos"
-	for (int i = 0; )
+	// Usar una estructura repetitiva para imprimir el contenido de todos los elementos de "punteroCharDos" con aritmética de punteros
+	for (i = 0; i < 5; i++){
+		printf("\nContenido [%i] de punteroCharDos = %c", i, *(punteroCharDos + i));
+	}
+	printf("\n");
 
+	// Usar una estructura repetitiva para imprimir el contenido de todos los elementos del puntero punteroCharDos con aritmética de punteros
+	for (int i = 0; i < 5; i++){
+		printf("\nContenido [%i] de punteroCharDos = %c", i, *(punteroCharDos + i));
+	}
 	return 0;
 }
